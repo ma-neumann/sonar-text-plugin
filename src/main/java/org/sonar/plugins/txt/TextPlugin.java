ed1e89db368589b9039c488dfd448346ebd5a8a8
Martin Neumann <mneumann@teco.edu>
2026-03-16 09:50:37 +0100

    Cosmetic renaming of plugin

--- src/main/java/org/sonar/plugins/txt/TextPlugin.java
+++ src/main/java/org/sonar/plugins/txt/TextPlugin.java
@@ -10,7 +10,7 @@ public final class TextPlugin implements Plugin {
   public static final String FILE_SUFFIXES_KEY = "sonar.txt.file.suffixes";
   public static final String REPOSITORY_KEY = "txt";
   public static final String LANGUAGE_KEY = "txt";
-  public static final String NAME = "Text (Community)";
+  public static final String NAME = "Text Analyser";
   public static final String SONAR_WAY_PROFILE_NAME = "Sonar way";
   public static final String SONAR_WAY_JSON_FILE_PATH = "org/sonar/l10n/txt/default_quality_profile/Sonar_way_profile.json";
 
